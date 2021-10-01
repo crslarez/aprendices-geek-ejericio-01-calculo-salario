4937127037dee5d1a8107b6b3734a15fbbe67318
Carlos Salazar <crslarez@gmail.com>
2021-10-01 19:07:15 -0400

    Boton calcular

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ const sueldoMaximo = 20000;
 let cantidadHora;
 let salarioSemanal;
 
-cantidadHora = document.getElementById("cantidadHora").value;
-    
-function myFunction(){ 
-if(cantidadHora <= 40){
-    salarioSemanal = cantidadHora * sueldoMinimo;
-}else{
-    salarioSemanal = cantidadHora * sueldoMaximo;
-}
-console.log(salarioSemanal);
+cantidadHora = document.getElementById(cantidadHora).value;
+console.log(cantidadHora);
+
+function myFunction(btnCalc) {
+  if (cantidadHora <= 40) {
+    cantidadHora * sueldoMinimo;
+  } else {
+    cantidadHora * sueldoMaximo;
+  }
 }
