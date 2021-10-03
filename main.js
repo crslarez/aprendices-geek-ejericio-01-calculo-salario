f84667b0c85ecffc2b8830b000bd61896a7cdfad
Carlos Salazar <crslarez@gmail.com>
2021-10-02 23:02:40 -0400

    Nuevas correcciones con asesoría

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
-const sueldoMinimo = 16000;
-const sueldoMaximo = 20000;
+const sueldoMin= 16000;
+const sueldoMax= 20000;
 
-let cantidadHora;
-let salarioSemanal;
 
-cantidadHora = document.getElementById(cantidadHora).value;
-console.log(cantidadHora);
 
-function myFunction(btnCalc) {
-  if (cantidadHora <= 40) {
-    cantidadHora * sueldoMinimo;
-  } else {
-    cantidadHora * sueldoMaximo;
-  }
-}
+const input = document.getElementById("user-input");
+const bnt = document.getElementById("calculate");
+const finalPriceContainer = document.getElementById('finalPrice');
+
+bnt.addEventListener("click", function() {
+    let totalHrs = Number(input.value.trim());
+    if(totalHrs <= 40){
+        
+        console.log("cada hora de trabajo equivale a" + sueldoMin )
+    }else{
+        console.log("cada hora de trabajo equivale a" + sueldoMax)
+    }
+});
