6df75ee477c9913ffa1a2f685e955245be429cd1
Carlos Salazar <crslarez@gmail.com>
2021-10-04 19:41:19 -0400

    Falta salida junto al total

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 const sueldoMin= 16000;
 const sueldoMax= 20000;
 
-
-
 const input = document.getElementById("user-input");
 const bnt = document.getElementById("calculate");
 const finalPriceContainer = document.getElementById('finalPrice');
 
-bnt.addEventListener("click", function() {
+let finalPrice = bnt.addEventListener("click", function() {
     let totalHrs = Number(input.value.trim());
     if(totalHrs <= 40){
         
-        console.log("cada hora de trabajo equivale a" + sueldoMin )
+      document.write('Tu sueldo total es: ' + totalHrs * sueldoMin)
+        //console.log("cada hora de trabajo equivale a" + sueldoMin)
     }else{
-        console.log("cada hora de trabajo equivale a" + sueldoMax)
+      document.write('Tu sueldo total es: ' + totalHrs * sueldoMax)
+        //console.log("cada hora de trabajo equivale a" + sueldoMax)
     }
 });
