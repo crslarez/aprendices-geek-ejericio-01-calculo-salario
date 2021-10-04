8036a2aa82d29045c660ec2b88ad096f3bbdcb8e
Carlos Salazar <crslarez@gmail.com>
2021-10-04 19:34:27 -0400

    Nuevo corregido

--- main.js
+++ main.js
@@ -1,24 +1,18 @@
-const sueldoMin = 16000;
-const sueldoMax = 20000;
-const cantHrs = 40;
+const sueldoMin= 16000;
+const sueldoMax= 20000;
+
+
 
 const input = document.getElementById("user-input");
 const bnt = document.getElementById("calculate");
-const finalPriceContainer = document.getElementById("finalPrice");
+const finalPriceContainer = document.getElementById('finalPrice');
 
-let totalSal = bnt.addEventListener ("click", function () {
-  let totalHrs = Number(input.value.trim());
-  if (totalHrs <= cantHrs) {
-
-    totalMin = totalHrs * sueldoMin;
-    console.log("Tu sueldo equivale a " + totalMin);
-  } else {
-    totalMax = totalHrs * sueldoMax;
-    console.log("Tu sueldo equivale a " + totalMax);
-  }
-  
-}
-);
-console.log(totalSal);
-
-//document.getElementById("totalImp").innerHTML = "Cambio de texto";
+bnt.addEventListener("click", function() {
+    let totalHrs = Number(input.value.trim());
+    if(totalHrs <= 40){
+        
+        console.log("cada hora de trabajo equivale a" + sueldoMin )
+    }else{
+        console.log("cada hora de trabajo equivale a" + sueldoMax)
+    }
+});
