9077c8dc3c1a04de3674d2551e9f649a84a8bb14
Carlos Salazar <crslarez@gmail.com>
2021-10-04 11:56:32 -0400

    Colocando el valor final

--- main.js
+++ main.js
@@ -1,21 +1,24 @@
-const sueldoMin= 16000;
-const sueldoMax= 20000;
-
-
+const sueldoMin = 16000;
+const sueldoMax = 20000;
+const cantHrs = 40;
 
 const input = document.getElementById("user-input");
 const bnt = document.getElementById("calculate");
-const finalPriceContainer = document.getElementById('finalPrice');
+const finalPriceContainer = document.getElementById("finalPrice");
 
-bnt.addEventListener("click", function() {
-    let totalHrs = Number(input.value.trim());
-    if(totalHrs <= 40){
-        totalMin = totalHrs * sueldoMin
-        console.log("Tu sueldo equivale a " + totalMin )
-    }else{
-        totalMax = totalHrs * sueldoMax
-        console.log("Tu sueldo equivale a " + totalMax)
-    }
-    
+let totalSal = bnt.addEventListener ("click", function () {
+  let totalHrs = Number(input.value.trim());
+  if (totalHrs <= cantHrs) {
+
+    totalMin = totalHrs * sueldoMin;
+    console.log("Tu sueldo equivale a " + totalMin);
+  } else {
+    totalMax = totalHrs * sueldoMax;
+    console.log("Tu sueldo equivale a " + totalMax);
+  }
+  
 }
 );
+console.log(totalSal);
+
+//document.getElementById("totalImp").innerHTML = "Cambio de texto";
