4b167456f53263f53b304d51153ffa5f55ab0554
Carlos Salazar <crslarez@gmail.com>
2021-10-11 02:00:25 -0400

    Falta CSS

--- main.js
+++ main.js
@@ -5,17 +5,20 @@ let totalHrs;
 
 const input = document.getElementById("user-input");
 const bnt = document.getElementById("calculate");
-const value_hrs = document.getElementById("value_hrs");
+const value_hrs = document.getElementById("valueHrs");
 const sueldoTotal = document.getElementById("sueldo");
 
 bnt.addEventListener("click", function () {
   totalHrs = Number(input.value.trim());
-  
+
   if (totalHrs <= HrsRef) {
     let honorarios = totalHrs * sueldoMin;
+
     value_hrs.innerText = sueldoMin;
     sueldoTotal.innerText = honorarios;
+
   } else {
+    
     let honorarios = totalHrs * sueldoMax;
     value_hrs.innerText = sueldoMax;
     sueldoTotal.innerText = honorarios;
